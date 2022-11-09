e8732cda9d4b03269ab0ca01ece81876d8f97a41
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-11-09 16:41:49 +0300

    added files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//djdjddd
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'welcome to api' });
 });
