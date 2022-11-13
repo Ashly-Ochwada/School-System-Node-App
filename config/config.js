3d4072242064fd09555266f1c50e0fdda5b8875e
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-11-13 20:11:33 +0300

    env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+// const path = require('path');
+// const dotenv = require('dotenv');
+// dotenv.config({
+//   path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+// });
 
 module.exports ={
   development: {
