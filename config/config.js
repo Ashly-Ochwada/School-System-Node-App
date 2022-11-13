497e0d3ea1c212f4edd37d83cbcf2987ef2f66db
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-11-13 21:02:01 +0300

    env file

--- config/config.js
+++ config/config.js
@@ -1,31 +1,66 @@
-require('dotenv').config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+// require('dotenv').config();
+// const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 
+
+// module.exports ={
+//   development: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: "nodeapp",
+//     host: DB_HOST,
+//     dialect: "postgres"
+//   },
+  
+//   test: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: "sequelize_database_test",
+//     host: DB_HOST,
+//     dialect: "postgres"
+//   },
+//   production: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: "sequelize_database_prod",
+//     host: DB_HOST,
+//     dialect: "postgres"
+//   }
+// }
+
+
+
+require('dotenv').config();
+// const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 
 module.exports ={
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    // use_env_variable: true,
     database: "nodeapp",
-    host: DB_HOST,
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
-  
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: "sequelize_database_test",
-    host: DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    // use_env_variable: true,
+    database: "nodeapp",
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
+  // production: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   use_env_variable: true,
+  //   database: "nodeapp",
+  //   host: process.env.DB_HOST,
+  //   dialect: "postgres"
+  // }
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: "sequelize_database_prod",
-    host: DB_HOST,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres"
   }
 }
-
 
 
