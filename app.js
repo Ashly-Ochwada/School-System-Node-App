fd79fe8b28ecf2b2eee16a066fcc16faf846fe26
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-11-13 20:55:21 +0300

    db

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- config/config.js
+++ config/config.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
-// const path = require('path');
-// const dotenv = require('dotenv');
-// dotenv.config({
-//   path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
-// });
+
 
 module.exports ={
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    // use_env_variable: true,
     database: "nodeapp",
     host: DB_HOST,
     dialect: "postgres"
